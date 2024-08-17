434a539e812f2b102bcee8c821d5110c1a84953d
pratikshya-gautam <gautam.pratikshya@outlook.com>
2024-08-17 12:05:57 +0200

    scout: add browser reset css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,9 @@ import type { HeadFC, PageProps } from "gatsby"
 import Timeline from "../components/timeline-style"
 import data from "../data/data.json"
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
+    <main>
       <Timeline data={data} title='Development of Hinduism Timeline Chart'/>
     </main>
   )
