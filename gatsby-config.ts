162cfffa2e07b980a1541ef5fcb0d26024c1bdbe
pratikshya-gautam <gautam.pratikshya@outlook.com>
2024-08-17 12:51:29 +0200

    move  pathPrefix root config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,9 +5,9 @@ const config: GatsbyConfig = {
     title: `Timeline Site`,
     siteUrl: `https://github.com/pratikshya-gautam`,
     author: `https://pratikshya.is-a.dev`,
-    pathPrefix: '/timeline'
 
   },
+  pathPrefix: '/timeline',
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
